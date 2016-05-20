f7493f4cd2d5965f2cc00d76b551ebc69b1115be
Tomasz Nieżurawski <tommaqs@gmail.com>
2016-05-20 13:42:57 +0200

    Added: Enable option in API to control scrolling of slimscroll

--- README.md
+++ README.md
@@ -1,5 +1,25 @@
 # What is slimScroll?
 
+### EDIT:
+
+Still all credits to Piotr Rochala.
+
+At 20.05.2016 tniezurawski added enable/disable option to slimscroll to get control over it.
+
+#### Usage
+
+1. save the reference to the object:
+
+- `var myFirstScroll = $('.elements').slimScroll(options).get(0);`
+
+2. From whatever moment you prefer, please use:
+
+- `myFirstScroll.enabled = false;` - to stop slimscroll from scrolling
+- `myFirstScroll.enabled = true;` - to let the slimscroll scrolling again
+
+
+### ORIGINAL README:
+
 slimScroll is a small jQuery plugin that transforms any div into a scrollable area with a nice scrollbar - similar to the one Facebook and Google started using in their products recently. slimScroll doesn't occupy any visual space as it only appears on a user initiated mouse-over. User can drag the scrollbar or use mouse-wheel to change the scroll value.
 
 Demo and deocumentation available here: [jQuery slimScroll docs](http://rocha.la/jQuery-slimScroll)
--- jquery.slimscroll.js
+++ jquery.slimscroll.js
@@ -4,6 +4,8 @@
  *
  * Version: 1.3.8
  *
+ * 20.04.2016
+ * - Enable/Disable option added by tniezurawski
  */
 (function($) {
 
@@ -76,7 +78,10 @@
         borderRadius: '7px',
 
         // sets border radius of the rail
-        railBorderRadius : '7px'
+        railBorderRadius : '7px',
+
+        // start and stops scrolling
+        enabled: true
       };
 
       var o = $.extend(defaults, options);
@@ -84,74 +89,76 @@
       // do it for every element that matches selector
       this.each(function(){
 
-      var isOverPanel, isOverBar, isDragg, queueHide, touchDif,
-        barHeight, percentScroll, lastScroll,
-        divS = '<div></div>',
-        minBarHeight = 30,
-        releaseScroll = false;
+        var isOverPanel, isOverBar, isDragg, queueHide, touchDif,
+            barHeight, percentScroll, lastScroll,
+            divS = '<div></div>',
+            minBarHeight = 30,
+            releaseScroll = false;
 
         // used in event handlers and for better minification
         var me = $(this);
 
+        this.enabled = o.enabled;
+
         // ensure we are not binding it again
         if (me.parent().hasClass(o.wrapperClass))
         {
-            // start from last bar position
-            var offset = me.scrollTop();
+          // start from last bar position
+          var offset = me.scrollTop();
 
-            // find bar and rail
-            bar = me.siblings('.' + o.barClass);
-            rail = me.siblings('.' + o.railClass);
+          // find bar and rail
+          bar = me.siblings('.' + o.barClass);
+          rail = me.siblings('.' + o.railClass);
 
-            getBarHeight();
+          getBarHeight();
 
-            // check if we should scroll existing instance
-            if ($.isPlainObject(options))
-            {
-              // Pass height: auto to an existing slimscroll object to force a resize after contents have changed
-              if ( 'height' in options && options.height == 'auto' ) {
-                me.parent().css('height', 'auto');
-                me.css('height', 'auto');
-                var height = me.parent().parent().height();
-                me.parent().css('height', height);
-                me.css('height', height);
-              } else if ('height' in options) {
-                var h = options.height;
-                me.parent().css('height', h);
-                me.css('height', h);
-              }
-
-              if ('scrollTo' in options)
-              {
-                // jump to a static point
-                offset = parseInt(o.scrollTo);
-              }
-              else if ('scrollBy' in options)
-              {
-                // jump by value pixels
-                offset += parseInt(o.scrollBy);
-              }
-              else if ('destroy' in options)
-              {
-                // remove slimscroll elements
-                bar.remove();
-                rail.remove();
-                me.unwrap();
-                return;
-              }
-
-              // scroll content by the given offset
-              scrollContent(offset, false, true);
+          // check if we should scroll existing instance
+          if ($.isPlainObject(options))
+          {
+            // Pass height: auto to an existing slimscroll object to force a resize after contents have changed
+            if ( 'height' in options && options.height == 'auto' ) {
+              me.parent().css('height', 'auto');
+              me.css('height', 'auto');
+              var height = me.parent().parent().height();
+              me.parent().css('height', height);
+              me.css('height', height);
+            } else if ('height' in options) {
+              var h = options.height;
+              me.parent().css('height', h);
+              me.css('height', h);
             }
 
-            return;
+            if ('scrollTo' in options)
+            {
+              // jump to a static point
+              offset = parseInt(o.scrollTo);
+            }
+            else if ('scrollBy' in options)
+            {
+              // jump by value pixels
+              offset += parseInt(o.scrollBy);
+            }
+            else if ('destroy' in options)
+            {
+              // remove slimscroll elements
+              bar.remove();
+              rail.remove();
+              me.unwrap();
+              return;
+            }
+
+            // scroll content by the given offset
+            scrollContent(offset, false, true);
+          }
+
+          return;
         }
         else if ($.isPlainObject(options))
         {
-            if ('destroy' in options)
-            {
-            	return;
-            }
+          if ('destroy' in options)
+          {
+            return;
+          }
         }
 
         // optionally set height to the parent's height
@@ -159,13 +166,13 @@
 
         // wrap content
         var wrapper = $(divS)
-          .addClass(o.wrapperClass)
-          .css({
-            position: 'relative',
-            overflow: 'hidden',
-            width: o.width,
-            height: o.height
-          });
+            .addClass(o.wrapperClass)
+            .css({
+              position: 'relative',
+              overflow: 'hidden',
+              width: o.width,
+              height: o.height
+            });
 
         // update style for the div
         me.css({
@@ -176,35 +183,35 @@
 
         // create scrollbar rail
         var rail = $(divS)
-          .addClass(o.railClass)
-          .css({
-            width: o.size,
-            height: '100%',
-            position: 'absolute',
-            top: 0,
-            display: (o.alwaysVisible && o.railVisible) ? 'block' : 'none',
-            'border-radius': o.railBorderRadius,
-            background: o.railColor,
-            opacity: o.railOpacity,
-            zIndex: 90
-          });
+            .addClass(o.railClass)
+            .css({
+              width: o.size,
+              height: '100%',
+              position: 'absolute',
+              top: 0,
+              display: (o.alwaysVisible && o.railVisible) ? 'block' : 'none',
+              'border-radius': o.railBorderRadius,
+              background: o.railColor,
+              opacity: o.railOpacity,
+              zIndex: 90
+            });
 
         // create scrollbar
         var bar = $(divS)
-          .addClass(o.barClass)
-          .css({
-            background: o.color,
-            width: o.size,
-            position: 'absolute',
-            top: 0,
-            opacity: o.opacity,
-            display: o.alwaysVisible ? 'block' : 'none',
-            'border-radius' : o.borderRadius,
-            BorderRadius: o.borderRadius,
-            MozBorderRadius: o.borderRadius,
-            WebkitBorderRadius: o.borderRadius,
-            zIndex: 99
-          });
+            .addClass(o.barClass)
+            .css({
+              background: o.color,
+              width: o.size,
+              position: 'absolute',
+              top: 0,
+              opacity: o.opacity,
+              display: o.alwaysVisible ? 'block' : 'none',
+              'border-radius' : o.borderRadius,
+              BorderRadius: o.borderRadius,
+              MozBorderRadius: o.borderRadius,
+              WebkitBorderRadius: o.borderRadius,
+              zIndex: 99
+            });
 
         // set position
         var posCss = (o.position == 'right') ? { right: o.distance } : { left: o.distance };
@@ -281,8 +288,8 @@
           // prevent scrolling the page if necessary
           if(!releaseScroll)
           {
-  		      e.originalEvent.preventDefault();
-		      }
+            e.originalEvent.preventDefault();
+          }
           if (e.originalEvent.touches.length)
           {
             // see how far user swiped
@@ -318,7 +325,7 @@
         function _onWheel(e)
         {
           // use mouse wheel only when mouse is over
-          if (!isOverPanel) { return; }
+          if (!isOverPanel || !this.enabled) { return; }
 
           var e = e || window.event;
 
@@ -327,6 +334,7 @@
           if (e.detail) { delta = e.detail / 3; }
 
           var target = e.target || e.srcTarget || e.srcElement;
+
           if ($(target).closest('.' + o.wrapperClass).is(me.parent())) {
             // scroll content
             scrollContent(delta, true);
@@ -425,8 +433,8 @@
             // publish approporiate event
             if (lastScroll != percentScroll)
             {
-                var msg = (~~percentScroll == 0) ? 'top' : 'bottom';
-                me.trigger('slimscroll', msg);
+              var msg = (~~percentScroll == 0) ? 'top' : 'bottom';
+              me.trigger('slimscroll', msg);
             }
           }
           else
